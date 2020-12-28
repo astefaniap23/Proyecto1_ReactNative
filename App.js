e7a9b0e6ce563c9cec8151dd75c8ac7b18def807
albaportillo <aportillo@consisint.com>
2020-12-28 11:15:20 -0500

    Grid two columns

--- App.js
+++ App.js
@@ -2,20 +2,84 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+var altoCaja=150;
+var anchoCaja=150;
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={miestilo.contenedor}>
+ 
+          <View style={miestilo.caja01}>
+            
+          </View>
+          <View style={miestilo.caja01}>
+           
+          </View>
+          <View style={miestilo.caja01}>
+          
+          </View>
+          <View style={miestilo.caja02}>
+           
+          </View>
+          <View style={miestilo.caja03}>
+           
+          </View>
+          <View style={miestilo.caja03}>
+           
+          </View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const miestilo = StyleSheet.create({
+  contenedor: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'column',
+    justifyContent:'center',
+    backgroundColor:'steelblue'
+    
   },
-});
+  caja01:{
+    marginTop:10,
+    height: altoCaja,
+    width: anchoCaja,
+    backgroundColor:"#9c9c9c",
+    opacity:0.8,
+    height:100,
+    marginLeft:20,
+    marginRight:20,
+    paddingTop:10,
+    backgroundColor:'orange',
+    borderColor:'yellow',
+  },
+  caja02:{
+    marginTop:-320,
+    height: altoCaja,
+    width: anchoCaja,
+    backgroundColor:"#9c9c9c",
+    opacity:0.8,
+    height:100,
+    marginLeft:anchoCaja + 50,
+    marginRight:anchoCaja + 50,
+    backgroundColor:'orange',
+    borderColor:'yellow',
+  },
+  caja03:{
+    marginTop:10,
+    height: altoCaja,
+    width: anchoCaja,
+    backgroundColor:"#9c9c9c",
+    opacity:0.8,
+    height:100,
+    marginLeft:anchoCaja + 50,
+    marginRight:anchoCaja + 50,
+    backgroundColor:'orange',
+    borderColor:'yellow',
+  },
+
+
+
+
+  
+})
